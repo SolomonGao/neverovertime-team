bd6445d91723fc5f9626fde9f3641bbaaa2651a7
Lindsay Garner <gar21034n@byui.edu>
2022-10-08 09:46:09 -0600

    second window

--- TKtest.py
+++ TKtest.py
@@ -5,6 +5,7 @@ from tkinter import filedialog as fd
 from tkinter.messagebox import showinfo
 from tkinter.ttk import *
 from tkinter import *
+import os
 
 
 # create the root window
@@ -64,10 +65,48 @@ def openWindow():
     myLabel = Label(new, text="Add some thing")
     myLabel.grid(row=0, column=0)
 
+def get_data():
+       folder_name = entry.get()
+       # print(folder_name)
+       return folder_name
+
+def create_subfolder(text):
+       source_path = filedialog.askdirectory(title='Select the Parent Directory')
+       path = os.path.join(source_path, get_data())
+       os.makedirs(path)
+       print(path)
+
+def clear_data():
+       entry.delete(0, END) 
+
+def folder_created_message():
+   label.Label(window2_main, text="New Folder was created", bg="white")
+
+
+
 def openWindow2():
     root.destroy()
     window2_main = Tk()
-    Label(window2_main, text="Bye Bye").pack()
+    Label(window2_main, text="New").pack()
+    # Import the required libraries
+    # Import the required libraries
+
+     # Create an instance of tkinter frame or window
+
+    entry = Entry(window2_main, width= 42)
+    entry.pack()
+    
+    Label(
+       window2_main,
+       text="Name New Folder", 
+       padx=10, 
+       pady=5,
+       ).pack()
+
+    button2 = Button(window2_main,text="Select New Folder Location", command=lambda: create_subfolder(get_data())).pack()
+
+    button3 = Button(window2_main, text="Clear", command=clear_data ).pack()
+
     window2_main.mainloop()
 
 
